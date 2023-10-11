08fb6f9821c9ea01de8858ea02a5fe647bf0999c
Tomi <somitamas1995@gmail.com>
2023-10-11 17:15:07 +0000

    Add dashboard for logged in user and wire it up

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
 
 
 def index(request):

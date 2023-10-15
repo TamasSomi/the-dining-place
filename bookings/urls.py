35b0d6db70a7b2ffe8a7e6ef4c492537edc78915
Tomi <somitamas1995@gmail.com>
2023-10-15 13:48:27 +0000

    Add delete and edit options for bookings

--- bookings/urls.py
+++ bookings/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('booking_success/', views.booking_success, name='booking_success'),
+    path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
+    path('edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Booking
 from .forms import BookingForm
-from .models import Booking 
 
 
 @login_required
@@ -32,3 +32,31 @@ def index(request):
 
 def booking_success(request):
     return render(request, 'booking_success.html')
+
+
+def delete_booking(request, booking_id):
+    booking = get_object_or_404(Booking, pk=booking_id)
+
+    if request.method == 'POST':
+        if 'confirm' in request.POST:
+            booking.delete()
+            return redirect('dashboard')
+        elif 'cancel' in request.POST:
+            return redirect('dashboard')
+
+    return render(request, 'delete_booking.html', {'booking': booking})
+
+
+def edit_booking(request, booking_id):
+    booking = get_object_or_404(Booking, pk=booking_id)
+
+    if request.method == 'POST':
+        form = BookingForm(request.POST, instance=booking)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+
+    else:
+        form = BookingForm(instance=booking)
+
+    return render(request, 'edit_booking.html', {'form': form, 'booking': booking})
